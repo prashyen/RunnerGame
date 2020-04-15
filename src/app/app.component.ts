4391f7e3a04b0e395f23163900d6094a1d32735a
prashyen <37853402+prashyen@users.noreply.github.com>
2020-04-15 00:56:06 -0400

    added left and right motion to character

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
 import { ViewChild, ElementRef } from '@angular/core';
 import { Landscape } from './landscape';
 import "@angular/compiler";
 import { Character } from './character';
 import { timer } from 'rxjs';
 import { Tree } from './tree';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-root',
@@ -16,30 +17,63 @@ import { Tree } from './tree';
 export class AppComponent implements OnInit {
 
   @ViewChild('myCanvas', { static: true })
-  canvas: ElementRef<HTMLCanvasElement>;  
+  canvas: ElementRef<HTMLCanvasElement>;
   private currDate: Date;
   private ctx: CanvasRenderingContext2D;
-private x:boolean;
+
+  keyBoard: Number;
+  moveLeft: boolean;
+  moveRight: boolean;
+  moveDown: boolean;
+  moveUp: boolean;
+  toggleLeg: boolean;
+  legSpeed: number = 60;
+
   ngOnInit(): void {
     this.ctx = this.canvas.nativeElement.getContext('2d');
-    ///Sharpen shape edges, code from https://developer.mozilla.org/en-US/docs/Web/API/Window/devicePixelRatio
-      // Set display size (css pixels).
-      this.canvas.nativeElement.style.width = window.innerWidth+ "px";
-      this.canvas.nativeElement.style.height = window.innerHeight + "px";
-
-      // Set actual size in memory (scaled to account for extra pixel density).
-      var scale = window.devicePixelRatio; // Change to 1 on retina screens to see blurry canvas.
-      this.canvas.nativeElement.width =window.innerWidth * scale;
-      this.canvas.nativeElement.height = window.innerHeight * scale;
-
-      // Normalize coordinate system to use css pixels.
-      this.ctx.scale(scale, scale);
-    ///
+    this.updateDisplayRatio();
     this.animate();
+    this.monitorKeys();
   }
 
-  animate() {
-    
+
+  monitorKeys() {
+    window.addEventListener('keyup', keyup)
+    window.addEventListener('keydown', keydown)
+    function keyup(e:KeyboardEvent) {
+      if (e.keyCode == 37) {
+        this.moveLeft = false;
+      } else if (e.keyCode == 39) {
+        this.moveRight = false;
+      }
+    }
+    function keydown(e:KeyboardEvent) {
+      this.keyBoard = e.keyCode
+      if (this.keyBoard == 37) {
+        this.moveLeft = true;
+      } else if (this.keyBoard == 39) {
+        this.moveRight = true;
+      }
+    }
+
+  }
+
+  updateDisplayRatio() {
+    ///Sharpen shape edges, code from https://developer.mozilla.org/en-US/docs/Web/API/Window/devicePixelRatio
+    // Set display size (css pixels).
+    this.canvas.nativeElement.style.width = window.innerWidth + "px";
+    this.canvas.nativeElement.style.height = window.innerHeight + "px";
+    // Set actual size in memory (scaled to account for extra pixel density).
+    var scale = window.devicePixelRatio; // Change to 1 on retina screens to see blurry canvas.
+    this.canvas.nativeElement.width = window.innerWidth * scale;
+    this.canvas.nativeElement.height = window.innerHeight * scale;
+    // Normalize coordinate system to use css pixels.
+    this.ctx.scale(scale, scale);
+    ///
+  }
+
+  private animate() {
+
     this.ctx.fillStyle = 'brown';
     const square = new Landscape(this.ctx);
     square.draw();
@@ -47,14 +81,14 @@ private x:boolean;
     const tree = new Tree(this.ctx);
     tree.draw();
 
-    this.x = false;
+    this.toggleLeg = false;
     const char = new Character(this.ctx);
-    setInterval(function(){ 
-      console.log(this.x);
-      char.draw(this.x,false, false,  false, true);
-      this.x=(!this.x);
-    }, 150);
-    
-    
+
+    setInterval(function () {
+      char.draw(this.toggleLeg, this.moveUp, this.moveDown, this.moveLeft, this.moveRight);
+      this.toggleLeg = (!this.toggleLeg);
+    }, this.legSpeed);
+
+
   }
 }
--- src/app/character.ts
+++ src/app/character.ts
@@ -1,49 +1,106 @@
 export class Character {
-    private color = '#808080';
-    private x = 20;
-    private characterHeight= 50;
-    private characterWidth= 20;
-    private y = (this.ctx.canvas.height-(this.ctx.canvas.height/5)*2 -20)-this.characterHeight;
-    constructor(private ctx: CanvasRenderingContext2D) {}
-  
-    public draw(toggleLeg:boolean, moveUp:boolean, moveDown:boolean, moveLeft:boolean, moveRight:boolean) {
-    if(moveLeft){
-        this.ctx.clearRect(this.x, this.y, 1, 12);
-        this.x = this.x-1;
-    }
-    if(moveRight){
-        this.ctx.clearRect(this.x, this.y-this.characterWidth/2, 1,  this.characterHeight-6+this.characterWidth/2+16);
-        this.ctx.clearRect(this.x, this.y+this.characterHeight-6,9, 16);
-        this.ctx.clearRect(this.x+8+3, this.y+this.characterHeight-6,+this.characterWidth-6, 16);
-        this.x=this.x+1;
-    }
-      this.ctx.fillStyle = this.color;
-      //body
-      //this.ctx.ellipse(this.x+10, this.y, this.characterWidth/2, this.characterWidth/2, 0,360, 1,false);
-      //this.ctx.fill();
-      this.ctx.fillRect(this.x, this.y, this.characterWidth, this.characterHeight-6);
+  private color = '#808080';
+  private x = 20;
+  private speed = 5;
+  private characterBodyHeight = 44;
+  private characterLegHeight = 16;
+  private characterBodyWidth = 20;
+  private y = (this.ctx.canvas.height - (this.ctx.canvas.height / 5) * 2 - 10) - this.characterBodyHeight - this.characterLegHeight;
+  constructor(private ctx: CanvasRenderingContext2D) { }
+  private toggleLeg: Boolean;
 
-      //legs
-      if(toggleLeg){
-        this.ctx.clearRect(this.x+8, this.y+this.characterHeight-6, 4, 12);
-        this.ctx.fillRect(this.x+8, this.y+this.characterHeight-6, 3, 16);
-        this.ctx.clearRect(this.x +this.characterWidth-6,  this.y+this.characterHeight-6, 4, 16);
-        this.ctx.fillRect(this.x +this.characterWidth-6,  this.y+this.characterHeight-6, 3, 12);
-      }else{
-        this.ctx.clearRect(this.x+8, this.y+this.characterHeight-6, 4, 16);
-        this.ctx.fillRect(this.x+8, this.y+this.characterHeight-6, 3, 12);
-        this.ctx.clearRect(this.x +this.characterWidth-6,  this.y+this.characterHeight-6, 4, 12);
-        this.ctx.fillRect(this.x +this.characterWidth-6,  this.y+this.characterHeight-6, 3, 16);
-      }
-      //hand
-      this.ctx.fillStyle = '#636161';
-      this.ctx.fillRect(this.x+9, this.y+24, 2, 15);
-      
-      //face
-      this.ctx.fillStyle = '#000000';
-      this.ctx.fillRect(this.x+13, this.y+2, 3, 3);
-      this.ctx.fillRect(this.x+17, this.y+2, 3, 3);
-      this.ctx.fillRect(this.x+12, this.y+10, 8, 2);
+  public animateCharacter(toggleLeg: boolean, moveUp: boolean, moveDown: boolean, moveLeft: boolean, moveRight: boolean) {
+
+    this.toggleLeg = false;
+    setInterval(function () {
+      this.draw(this.toggleLeg, moveUp, moveDown, moveLeft, moveRight);
+      this.toggleLeg = (!this.toggleLeg);
+    }, 150);
+  }
+
+  public draw(toggleLeg: boolean, moveUp: boolean, moveDown: boolean, moveLeft: boolean, moveRight: boolean) {
+
+    if (moveRight) {
+      this.ctx.clearRect(this.x + this.characterBodyWidth, this.y, this.speed, this.characterBodyHeight + 1);
+      this.ctx.clearRect(this.x + 6, this.y + this.characterBodyHeight, this.speed + 6, this.characterLegHeight);
+      this.ctx.clearRect(this.x + this.characterBodyWidth - 6, this.y + this.characterBodyHeight, this.speed, this.characterLegHeight);
+
+      this.ctx.clearRect(this.x, this.y, this.speed, this.characterBodyHeight + 1);
+      this.ctx.clearRect(this.x + 8, this.y + this.characterBodyHeight, 4, this.characterLegHeight);
+      this.ctx.clearRect(this.x + this.characterBodyWidth - 6, this.y + this.characterBodyHeight, 4, this.characterLegHeight);
+
+      this.x = this.x + this.speed;
+      this.lookRight(toggleLeg);
+    } else if (moveLeft) {
+      this.ctx.clearRect(this.x + this.characterBodyWidth - 5, this.y, this.speed + 6, this.characterBodyHeight + 1);
+      this.ctx.clearRect(this.x + 10, this.y + this.characterBodyHeight, this.speed + 2, this.characterLegHeight);
+      this.ctx.clearRect(this.x + this.characterBodyWidth - 6, this.y + this.characterBodyHeight, this.speed, this.characterLegHeight);
+      this.x = this.x - this.speed;
+      this.lookLeft(toggleLeg);
+    }
+    else {
+      this.ctx.clearRect(this.x + this.characterBodyWidth, this.y, this.speed, this.characterBodyHeight + 1);
+      this.ctx.clearRect(this.x + 6, this.y + this.characterBodyHeight, this.speed + 2, this.characterLegHeight);
+      this.ctx.clearRect(this.x + this.characterBodyWidth - 4, this.y + this.characterBodyHeight, this.speed + 1, this.characterLegHeight);
+      this.ctx.clearRect(this.x + 12, this.y + this.characterBodyHeight, 2, this.characterLegHeight);
+      this.lookRight(toggleLeg);
     }
   }
-  
+
+
+  lookRight(toggleLeg: boolean) {
+    this.ctx.fillStyle = this.color;
+    //body
+    this.ctx.fillRect(this.x, this.y, this.characterBodyWidth, this.characterBodyHeight);
+
+    //legs
+    if (toggleLeg) {
+      this.ctx.clearRect(this.x + 8, this.y + this.characterBodyHeight, 4, this.characterLegHeight - 6);
+      this.ctx.fillRect(this.x + 8, this.y + this.characterBodyHeight, 3, this.characterLegHeight);
+      this.ctx.clearRect(this.x + this.characterBodyWidth - 6, this.y + this.characterBodyHeight, 4, this.characterLegHeight);
+      this.ctx.fillRect(this.x + this.characterBodyWidth - 6, this.y + this.characterBodyHeight, 3, this.characterLegHeight - 6);
+    } else {
+      this.ctx.clearRect(this.x + 8, this.y + this.characterBodyHeight, 4, this.characterLegHeight);
+      this.ctx.fillRect(this.x + 8, this.y + this.characterBodyHeight, 3, this.characterLegHeight - 6);
+      this.ctx.clearRect(this.x + this.characterBodyWidth - 6, this.y + this.characterBodyHeight, 4, this.characterLegHeight - 6);
+      this.ctx.fillRect(this.x + this.characterBodyWidth - 6, this.y + this.characterBodyHeight, 3, this.characterLegHeight);
+    }
+    //hand
+    this.ctx.fillStyle = '#636161';
+    this.ctx.fillRect(this.x + 9, this.y + 24, 2, 15);
+
+    //face
+    this.ctx.fillStyle = '#000000';
+    this.ctx.fillRect(this.x + 13, this.y + 2, 3, 3);
+    this.ctx.fillRect(this.x + 17, this.y + 2, 3, 3);
+    this.ctx.fillRect(this.x + 12, this.y + 10, 8, 2);
+  }
+
+  lookLeft(toggleLeg: boolean) {
+    this.ctx.fillStyle = this.color;
+    //body
+    this.ctx.fillRect(this.x, this.y, this.characterBodyWidth, this.characterBodyHeight);
+
+    //legs
+    if (toggleLeg) {
+      this.ctx.clearRect(this.x + 6, this.y + this.characterBodyHeight, 4, this.characterLegHeight - 4);
+      this.ctx.fillRect(this.x + 6, this.y + this.characterBodyHeight, 3, this.characterLegHeight);
+      this.ctx.clearRect(this.x + this.characterBodyWidth - 8, this.y + this.characterBodyHeight, 4, this.characterLegHeight);
+      this.ctx.fillRect(this.x + this.characterBodyWidth - 8, this.y + this.characterBodyHeight, 3, this.characterLegHeight - 4);
+    } else {
+      this.ctx.clearRect(this.x + 6, this.y + this.characterBodyHeight, 4, this.characterLegHeight);
+      this.ctx.fillRect(this.x + 6, this.y + this.characterBodyHeight, 3, this.characterLegHeight - 4);
+      this.ctx.clearRect(this.x + this.characterBodyWidth - 8, this.y + this.characterBodyHeight, 4, this.characterLegHeight - 4);
+      this.ctx.fillRect(this.x + this.characterBodyWidth - 8, this.y + this.characterBodyHeight, 3, this.characterLegHeight);
+    }
+    //hand
+    this.ctx.fillStyle = '#636161';
+    this.ctx.fillRect(this.x + 9, this.y + 24, 2, 15);
+
+    //face
+    this.ctx.fillStyle = '#000000';
+    this.ctx.fillRect(this.x, this.y + 2, 3, 3);
+    this.ctx.fillRect(this.x + 4, this.y + 2, 3, 3);
+    this.ctx.fillRect(this.x, this.y + 10, 8, 2);
+  }
+}
